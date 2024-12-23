927f5bea8d6d387bfedd8ac672deb9bd6bdebf82
Deeptansh Nagar <diptanshnagar@gmail.com>
2024-12-23 17:50:40 +0530

    hackerRank

---
+++ HackerRank/BirthdayCakeCandles.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Result {
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+        int max = Integer.MIN_VALUE;
+        int count = 0;
+
+        for (int height : candles) {
+            if (height > max) {
+                max = height;
+                count = 1;
+            } else if (height == max) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
